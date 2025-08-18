add05a10f70cbd555dd5e3ffff210edd12e966c3
mamonaco1973 <mamonaco1973@gmail.com>
2025-08-18 21:44:52 +0000

    fix destroy.sh

--- destroy.sh
+++ destroy.sh
@@ -3,16 +3,14 @@
 
 set -e
 
-# cd 02-servers
+cd 02-servers
 
-# default_domain=$(az rest --method get --url "https://graph.microsoft.com/v1.0/domains" --query "value[?isDefault].id" --output tsv)
-# echo "NOTE: Default domain for account is $default_domain"
-# vault=$(az keyvault list --resource-group ad-resource-group --query "[?starts_with(name, 'ad-key-vault')].name | [0]" --output tsv)
-# echo "NOTE: Key vault for secrets is $vault"
-# terraform init
-# terraform destroy -var="vault_name=$vault" -var="azure_domain=$default_domain" -auto-approve
+vault=$(az keyvault list --resource-group mini-ad-rg --query "[?starts_with(name, 'ad-key-vault')].name | [0]" --output tsv)
+echo "NOTE: Key vault for secrets is $vault"
+terraform init
+terraform destroy -var="vault_name=$vault" -auto-approve
 
-# cd ..
+cd ..
 
 cd 01-directory
 
