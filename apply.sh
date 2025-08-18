25765d3714240c0c82fe49222cc7ae785bce182c
mamonaco1973 <mamonaco1973@gmail.com>
2025-08-18 19:18:39 +0000

    fix apply.sh

--- apply.sh
+++ apply.sh
@@ -21,7 +21,7 @@ cd ..
 
 cd 02-servers
 
-vault=$(az keyvault list --resource-group ad-resource-group --query "[?starts_with(name, 'ad-key-vault')].name | [0]" --output tsv)
+vault=$(az keyvault list --resource-group mini-ad-rg --query "[?starts_with(name, 'ad-key-vault')].name | [0]" --output tsv)
 echo "NOTE: Key vault for secrets is $vault"
 terraform init
 terraform apply -var="vault_name=$vault" -auto-approve
