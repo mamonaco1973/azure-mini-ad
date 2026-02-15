8ecfee76cc226ba920607ca4e520b785481113d0
Mike Monaco <mamonaco1973@gmail.com>
2026-02-15 16:32:56 -0500

    2026 tech refresh

--- validate.sh
+++ validate.sh
@@ -84,7 +84,5 @@ echo ""
 printf "%-28s %s\n" "NOTE: Windows RDP Host:" "${windows_fqdn}"
 printf "%-28s %s\n" "NOTE: Linux SSH Host:"   "${linux_fqdn}"
 
-echo ""
-printf "%-28s %s\n" "NOTE:" "Validation complete."
-echo ""
+"
 
